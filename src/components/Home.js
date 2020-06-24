c41c1273f18c16c4bae5e75d98d6a93b2c2994cb
ZoeDj <studio3fun@gmail.com>
2020-06-24 13:50:15 -0400

    setState

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
 import Message from "./Message";
+import Counter from "./Counter";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Link to="/item-list">
         <button>Shop</button>
       </Link>
+      <Counter />
     </div>
   );
 };
