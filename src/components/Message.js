45138632527bc62e0dc0c2c058f7aec57047b202
ZoeDj <studio3fun@gmail.com>
2020-06-24 14:57:25 -0400

    biding handlers

--- src/components/Message.js
+++ src/components/Message.js
@@ -6,12 +6,16 @@ class Message extends Component {
     this.state = {
       message: "Welcome to our online store, ",
     };
+
+    this.changeMessage = this.changeMessage.bind(this);
   }
 
   changeMessage() {
     this.setState({
       message: "Thank you for subsribing, ",
     });
+    // "this" is undefined and biding is necessary
+    // console.log(this);
   }
 
   render() {
@@ -23,6 +27,9 @@ class Message extends Component {
           {this.state.message}
           {name} {/*this.props.name becomes name */}
         </h1>
+        {/* <button onClick={() => this.changeMessage()}>Subscribe</button> */}
+        {/* <button onClick={this.changeMessage.bind(this)}>Subscribe</button> */}
+        {/* method #3 is to bind in constructor */}
         <button onClick={() => this.changeMessage()}>Subscribe</button>
       </div>
     );
