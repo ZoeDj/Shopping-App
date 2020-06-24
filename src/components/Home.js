62f83ef8762c68f863bb9adc0fa833b796276bce
ZoeDj <studio3fun@gmail.com>
2020-06-24 12:30:38 -0400

    state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
+import Message from "./Message";
 
 const Home = () => {
   return (
     <div className="homepage">
-      <h1>Welcome to our online store</h1>
+      <h1>
+        <Message />
+      </h1>
       <Link to="/item-list">
         <button>Shop</button>
       </Link>
